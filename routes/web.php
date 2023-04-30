3feb655350659d29434ea14aa7b292d8fa428756
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-05-01 02:04:58 +0300

    AdminController created, user logout process integrated

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,18 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//? Admin All Routes
+Route::controller(AdminController::class)->group(function (){
+   Route::get('/admin/logout', 'destroy')->name('admin.logout');
+});
+
 
 Route::get('/dashboard', function () {
     return view('admin.index');
